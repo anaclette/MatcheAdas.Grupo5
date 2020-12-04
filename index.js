73369c77b2f196f06c8ca5542ac6fedb95500802
carokartu <carokrstulovich@gmail.com>
2020-12-04 20:23:55 -0300

    cambios en css

--- index.js
+++ index.js
@@ -1,60 +1,158 @@
+const body = document.body
 const grillaHTML = document.querySelector("#grilla")
-const dialogoNuevoJuego = document.querySelector(".dialogo.nuevo-juego")
-const botonNuevoJuego = document.querySelector("#boton-nuevo-juego")
+const dialogoNuevoJuego = document.querySelector("#dialogo-nuevo-juego")
+const botonesNuevoJuego = document.querySelectorAll("#boton-nuevo-juego")
 const botonReiniciarJuego = document.querySelector("#boton-reiniciar-juego")
 const botonBuscarMatch = document.querySelector("#boton-buscar-match")
 const botonModoFacil = document.querySelector("#boton-modo-facil")
 const botonModoNormal = document.querySelector("#boton-modo-normal")
 const botonModoDificil = document.querySelector("#boton-modo-dificil")
 const mosaicos = document.getElementsByClassName('mosaico')
-const items = ['🦝', '🦊', '🐺', '🍁', '🌲', '🍄', '🐻', '🐦', '🐨',]
+const items = ['🐺', '🦊', '🦝 ', '🐻 ', '🐨 ', '🐦', '🍄', '🌲', '🍁',]
+const tiempoRestanteHTML = document.querySelector("#tiempo-restante")
+const overlay = document.querySelector("#overlay")
+const dialogoJuegoTerminado = document.querySelector("#dialogo-juego-terminado")
+const botonAyuda = document.querySelector("#boton-ayuda")
+const botonIconoRestart = document.querySelector("#boton-restart")
+const dialogoBienvenida = document.querySelector("#dialogo-bienvenida")
+const dialogoReiniciarJuego = document.querySelector("#dialogo-reiniciar-juego")
+const botonAJugar = document.querySelector("#boton-empezar-juego")
+const ventanasDeDialogo = document.querySelectorAll(".dialogo")
+const botonCancelar = document.querySelector("#boton-cancelar")
 
-//Comportamiento general de modales
-const abrirModal = (elemento) => {
-    elemento.classList.remove('hiden')
+
+//Comportamiento general del overlay
+const abrirModal = () => {
     overlay.classList.remove('hidden')
+    body.classList.add('no-scroll')
 }
 
-const cerrarModal = (elemento) => {
-    elemento.classList.add('hiden')
-    overlay.classList.add('hiden')
+const cerrarModal = () => {
+    overlay.classList.add('hidden')
 
+    for (let dialogo of ventanasDeDialogo) {
+        dialogo.classList.add('hidden')
+    }
 }
 
+//Comportamiento general de modales y botones
+window.onload = () => {
+    dialogoBienvenida.classList.remove('hidden')
+}
+
+botonAyuda.onclick = () => {
+    abrirModal()
+    dialogoBienvenida.classList.remove('hidden')
+}
+
+botonIconoRestart.onclick = () => {
+    abrirModal()
+    dialogoReiniciarJuego.classList.remove('hidden')
+}
+
+botonAJugar.onclick = () => {
+    botonAJugar.parentElement.parentElement.classList.add('hidden')
+    dialogoNuevoJuego.classList.remove('hidden')
+}
+
+for (let boton of botonesNuevoJuego) {
+    boton.onclick = () => {
+        boton.parentElement.parentElement.classList.add('hidden')
+        dialogoNuevoJuego.classList.remove('hidden')
+    }
+}
+
+
+
+botonCancelar.onclick = () => {
+    cerrarModal()
+}
 
 
 //Pedir al usuario que elija la dificultad de la partida
 let nivelDificultad = '' //Almaceno niveles de dificultad para reutilizar luego
 
-
-
 botonModoFacil.onclick = () => {
     grillaFacil()
+    
+for (let mosaico of mosaicos) {
+    mosaico.onclick = () => {
+// PRIMER CLICK
+        console.log(mosaico);
+         clicks.push(mosaico)
+          for (let mosaico2 of mosaicos) {
+// SEGUNDO CLICK
+             mosaico2.onclick = () => {
+                 console.log(mosaico2);
+             clicks.push(mosaico2);
+             
+      }
+    }
+}
+}
+
     cuentaRegresiva()
-    nivelDificultad = 'facil' 
+    cerrarModal()
+    nivelDificultad = 'facil'
 }
 
 botonModoNormal.onclick = () => {
     grillaNormal()
     cuentaRegresiva()
+    cerrarModal()
     nivelDificultad = 'normal'
 }
 
 botonModoDificil.onclick = () => {
     grillaDificil()
     cuentaRegresiva()
+    cerrarModal()
     nivelDificultad = 'dificil'
 }
 
-//Empezar cuenta regresiva al crear un juego nuevo --------FUNCIONA JS, FALTA EL MAQUETADO DEL TIMER PARA PODER CONECTARLOS----
-let tiempoRestante = 30
+const revisarDificultadElegida = () => {
+    if (nivelDificultad === 'facil') {
+        grillaFacil()
+    }
+    else if (nivelDificultad === 'normal') {
+        grillaNormal()
+    }
+    else if (nivelDificultad === 'dificil') {
+        grillaDificil()
+    }
+}
+
+//Empezar cuenta regresiva al crear un juego nuevo --------FUNCIONA AL INICIO, TIENE BUG AL REINICIAR----
+let tiempoRestante = 3
 
 const cuentaRegresiva = () => {
-    tiempoRestante--
+    tiempoRestanteHTML.textContent = `0:${tiempoRestante}`
+    
     if (tiempoRestante > 0) {
+        tiempoRestante--
         setTimeout(cuentaRegresiva, 1000)
+        }
+    else {
+        tiempoRestante = 3
+        terminarJuego()
     }
-    console.log(tiempoRestante)
+
+}
+
+
+botonReiniciarJuego.onclick = () => {
+    console.log("Pepo")
+    revisarDificultadElegida()
+    //dialogoNuevoJuego.classList.remove('hidden')
+    cuentaRegresiva()
+    cerrarModal()
+
+}
+
+const terminarJuego = () => {
+    dialogoNuevoJuego.classList.add('hidden')
+    abrirModal()
+    dialogoJuegoTerminado.classList.remove('hidden')
 }
 
 // Crear una grilla en JS y en HTML con items aleatorios 
@@ -62,26 +160,26 @@ const cuentaRegresiva = () => {
 
 const grillaFacil = () => {
     do {
-    vaciarGrillaHTML()
-    generarGrilla(9, 9)
-    generarGrillaEnHTML(9, 9, items)
-    } while(hayMatchInicial())
+        vaciarGrillaHTML()
+        generarGrilla(9, 9)
+        generarGrillaEnHTML(9, 9, items)
+    } while (hayMatchInicial())
 }
 
 const grillaNormal = () => {
     do {
-    vaciarGrillaHTML()
-    generarGrilla(8, 8)
-    generarGrillaEnHTML(8, 8, items)
-    } while(hayMatchInicial())
+        vaciarGrillaHTML()
+        generarGrilla(8, 8)
+        generarGrillaEnHTML(8, 8, items)
+    } while (hayMatchInicial())
 }
 
 const grillaDificil = () => {
     do {
-    vaciarGrillaHTML()
-    generarGrilla(7, 7)
-    generarGrillaEnHTML(7, 7, items)
-    } while(hayMatchInicial())
+        vaciarGrillaHTML()
+        generarGrilla(7, 7)
+        generarGrillaEnHTML(7, 7, items)
+    } while (hayMatchInicial())
 }
 
 
@@ -105,11 +203,14 @@ const generarGrilla = (filas, columnas) => {
     }
     return grilla
 }
+//console.log(generarGrilla)
+
 
 const generarMosaicos = (x, y, array) => {
     const tamanio = 50
 
     const mosaico = document.createElement('div')
+    
     mosaico.classList.add('mosaico')
     mosaico.dataset.x = x
     mosaico.dataset.y = y
@@ -165,34 +266,8 @@ const hayMatchInicial = () => {
     }
 }
 
-
-// //--------------FALTA HACER----------------
-// //Opciones nuevo juego
-
-botonNuevoJuego.onclick = () => {
-    console.log(dialogoNuevoJuego)
-}
-// //-----------------------------------------
-
 const vaciarGrillaHTML = () => {
     grillaHTML.textContent = ''
-}
-
-botonReiniciarJuego.onclick = () => {
-    //grillaHTML.textContent = ''
-    revisarDificultadElegida()
-}
-
-const revisarDificultadElegida = () => {
-    if (nivelDificultad === 'facil') {
-        grillaFacil()
-    }
-    else if (nivelDificultad === 'normal') {
-        grillaNormal()
-    }
-    else if (nivelDificultad === 'dificil') {
-        grillaDificil()
-    }
 }
 
 
@@ -207,15 +282,20 @@ const revisarDificultadElegida = () => {
 // 
 
 //Seleccionar mosaico al hacer click en el
-const primerMosaicoSeleccionado = ''
-for (let mosaico of mosaicos) {
+let primerMosaicoSeleccionado= ''
+for (let i = 0; i < mosaicos.length; i++) {
+    primerMosaicoSeleccionado+= i
+    
+    console.log("soy un mosaico")
     mosaico.onclick = () => {
-        mosaico.classList.add('seleccionado')
-        const primerMosaicoSeleccionado = mosaico
-        console.log(primerMosaicoSeleccionado)
+     seleccionarMosaico()
     }
 }
 
+const seleccionarMosaico=()=>{
+    mosaico.classList.add('seleccionado')
+    console.log("pepo")
+}
 
 // desaparecen las frutas
 // elimino los elementos tanto en HTML como en JS 
@@ -227,7 +307,4 @@ for (let mosaico of mosaicos) {
 // obtener la cantidad de posciones vacias que tiene debajo
 // bajar el item esas pisiciones
 // rellenar posiciones restantes (las de mas arriba) con elementos al azar
-
-
-
 
